f28d74f27be63da88b25f3d45d5c2d080b2a3c22
Esteban-can <125224817+Esteban-can@users.noreply.github.com>
2023-03-14 18:50:08 -0600

    Primer_commit

---
+++ for_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+main()
+{
+	int contar=0;
+	for (int i=0;i<=10;i++)
+	{
+		cout<<i<<endl;
+		contar++;
+	}
+	    cout<<"Ciclos: "<<contar<<endl;
+		system("pause");
+	
+}
